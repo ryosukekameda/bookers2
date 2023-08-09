d6a45417d50abc29230c3eb4b90b2966008a2c4a
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-08-09 09:40:23 +0000

    08091840

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,43 @@
 class BooksController < ApplicationController
+  before_action :current_user, only: [:edit, :update]
+  
   def new
     @book = Book.new
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to 'books/show'
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path(@book.id)
   end
   
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
   
   def show
+    @book = Book.find(params[:id])
+    @user = current_user
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end  
+    
   private
   
   def book_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_08_070428) do
+ActiveRecord::Schema.define(version: 2023_08_09_063448) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2023_08_08_070428) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
