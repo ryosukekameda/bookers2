6c14a910b13fbe13046409acfa727bce26a162af
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-08-10 10:41:41 +0000

    08101941

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,9 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.user_id = current_user.id
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+    
   end
   
   def destroy
